01eb2701c94cdb8798b0088077c486666b020454
Anders Persson <anders@slaughterhouse.se>
2021-12-02 15:55:59 +0100

    211202

--- BlazorApp/Scheduler/JeevesScheduler.cs
+++ BlazorApp/Scheduler/JeevesScheduler.cs
@@ -8,7 +8,7 @@ namespace BlazorApp.Scheduler
     {
         public void LoadProjects()
         {
-            using(var cn = new SqlConnection("Server=got-sql01.forshemgruppen.se;Database=Gotenehus;User Id=sales_rw;Password=Int12345;"))
+            using(var cn = new SqlConnection("Server=xxx;Database=xxx;User Id=xxx;Password=xxx;"))
             {
                 // GET LAST 100 PROJECTS FROM JEEVES
                 var sql = "SELECT TOP(100) ProjCode as Project, ProjDescr as Description FROM prj ORDER BY RowCreatedDt DESC";
